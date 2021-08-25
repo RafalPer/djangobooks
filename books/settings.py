2ca7a6482be077889e1d18804db448de036c89ef
RafalPer <rafalpero1@gmail.com>
2021-08-25 20:05:00 +0200

    added allowed host

--- books/settings.py
+++ books/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+6*ipy-z=)k2y!*5*03te^$hq)js5s96hu)&*@dvnqa&rkss9t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['still-stream-12104.herokuapp.com/, '127.0.0.1']
 
 
 # Application definition
